74dda91f234ae274622ff658b911dac9219557e5
lalit-1729 <lakithakare54726@gmail.com>
2024-07-19 16:00:18 +0530

    config file corrected

--- ProductMicroservice/kafka-config.sh
+++ ProductMicroservice/kafka-config.sh
@@ -3,12 +3,17 @@
 cd ~
 cd /Users/thakre/kafka
 
-echo "Current working directory is ${pwd}"
+echo "Current working directory is $(pwd)"
 
 echo "Generating random-uuid"
-randomUUID = $(./bin/kafka-storage.sh)
+random_uuid=$(./bin/kafka-storage.sh random-uuid)
 
+echo "The random UUID is ${random_uuid}"
 
-./bin/kafka-storage.sh format -t ${randomUUID} -c config/kraft/server1.properties
-./bin/kafka-storage.sh format -t ${randomUUID} -c config/kraft/server2.properties
-./bin/kafka-storage.sh format -t ${randomUUID} -c config/kraft/server3.properties
+./bin/kafka-storage.sh format -t ${random_uuid} -c config/kraft/server1.properties
+./bin/kafka-storage.sh format -t ${random_uuid} -c config/kraft/server2.properties
+./bin/kafka-storage.sh format -t ${random_uuid} -c config/kraft/server3.properties
+
+#//./bin/kafka-server-start.sh config/kraft/server2.properties
+#//./bin/kafka-server-start.sh config/kraft/server3.properties
+#//./bin/kafka-server-start.sh config/kraft/server1.properties
