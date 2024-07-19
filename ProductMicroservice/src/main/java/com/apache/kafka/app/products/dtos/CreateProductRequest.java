b903091360205c8391e4ba5c943b250287d37ea4
lalit-1729 <lakithakare54726@gmail.com>
2024-07-19 16:01:33 +0530

    Add Dto and event

---
+++ ProductMicroservice/src/main/java/com/apache/kafka/app/products/dtos/CreateProductRequest.java
@@ -0,0 +1,18 @@
+package com.apache.kafka.app.products.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateProductRequest {
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+}
---
+++ ProductMicroservice/src/main/java/com/apache/kafka/app/products/event/ProductCreatedEvent.java
@@ -0,0 +1,19 @@
+package com.apache.kafka.app.products.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductCreatedEvent {
+    private String id;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+}
